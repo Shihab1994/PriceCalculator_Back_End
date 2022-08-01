b6caf6a640fbf5c5031e24d805d28902c49943d9
APU_Mahfuzul <mahfuzul@eatlbd.com>
2022-08-01 13:21:13 +0600

    99X_Back_End(Price Calculator)

--- PriceCalculator/src/main/java/com/test/PriceCalculator/PriceCalculatorApplication.java
+++ PriceCalculator/src/main/java/com/test/PriceCalculator/PriceCalculatorApplication.java
@@ -2,12 +2,26 @@ package com.test.PriceCalculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableWebMvc
+@EnableSwagger2
 public class PriceCalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PriceCalculatorApplication.class, args);
+	}
+
+	@Bean
+	public InternalResourceViewResolver defaultViewResolver() {
+		return new InternalResourceViewResolver();
 	}
 
 }
--- PriceCalculator/src/main/java/com/test/PriceCalculator/controller/PriceCalculationController.java
+++ PriceCalculator/src/main/java/com/test/PriceCalculator/controller/PriceCalculationController.java
@@ -3,6 +3,8 @@ package com.test.PriceCalculator.controller;
 import com.test.PriceCalculator.model.Price;
 import com.test.PriceCalculator.model.Product;
 import com.test.PriceCalculator.service.PriceCalculationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,27 +15,32 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/calculator")
+@Api(tags = "Price Calculator")
 public class PriceCalculationController {
 
     @Autowired
     private PriceCalculationService service;
 
     @GetMapping("/productList")
+    @ApiOperation(value = "product list", response = Product.class)
     public List<Product> getProductList() {
         return service.getProductList();
     }
 
     @GetMapping("/priceList")
+    @ApiOperation(value = "price list", response = Price.class)
     public List<Price> getPriceList() {
         return service.getPriceList();
     }
 
     @GetMapping("/price")
+    @ApiOperation(value = "product price", response = Price.class)
     public Price getPriceById(@RequestParam int productId) {
         return service.getPriceById(productId);
     }
 
     @GetMapping("/totalPrice")
+    @ApiOperation(value = "Total price", response = Double.class)
     public double getTotalProductPrice(@RequestParam int productId, @RequestParam int orderUnits) {
         return service.getPriceOfProduct(productId, orderUnits);
     }
